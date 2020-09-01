628da0a5fead0a16cb5a11f43bd48bb72d308a30
Pedro Martinez <martinezpt3@gmail.com>
2020-08-31 21:17:43 -0400

    New Commit

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
-'use strict'
+function allowDrop(ev)
+{
+    ev.preventDefault();
+}
 
-
-// This is a single-line comment. This was cloned.
-
-/* Here's a
-multi-line
-comment. */
-
-// console.log('This code is no longer active')
-
-// <!-- This is an HTML comment. -->
-
-/* CSS comments are just like multi-line 
-JavaScript ones.*/
-
-let today = new Date();
-let formatDate = today.toDateString();
-let selectElement = document.getElementById('date');
-selectElement.innerHTML = formatDate;
-
-console.log('Here\'s a hidden message');
+function dragStart(ev)
+{
+    id=ev.target.id;
+    alert(id);
+}
